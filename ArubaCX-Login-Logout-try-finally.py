9b44007d2102bd2ac2723185e6eebbce8e9c8213
rameshroy83 <rameshroy83@gmail.com>
2025-07-30 16:36:57 +0530

    Login-Logout-with-try

---
+++ ArubaCX-Login-Logout-try-finally.py
@@ -0,0 +1,17 @@
+import urllib3
+import requests
+
+session = requests.session()
+
+ip_addr = "10.97.169.5"
+cred = {"username": "admin", "password": "HTS!ndi@"}
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+try:
+    login = session.post(f"https://{ip_addr}/rest/v10.10/login", data=cred, verify=False)
+    print(login.status_code)
+    print(login.cookies)
+
+finally:
+    logout = session.post(f"https://{ip_addr}/rest/v10.10/logout", data=cred, verify=False)
+    print(logout.status_code)
